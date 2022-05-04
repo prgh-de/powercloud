a21c54b1a5a499fe66da0630177e79d22f5fef04
Bye, Christoph-Bernhard <Christoph-Bernhard.Bye@adesso.de>
2022-05-04 13:01:12 +0200

    added font awesome icons, links, copyright

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,67 @@
+import styles from "./Footer.module.css"
 
-export default function Footer(){
-    return(
-        <>
+export default function Footer() {
+    // copyRight
+    // impressum
+    // datenschutz
+
+//seit bis
+    return (
+        <div className={styles.footer}>
+
+
             Footer
-        </>
+
+            <p className={styles.pTag}>&copy; {new Date(2012, 1, 1).getFullYear()}-{new Date().getFullYear()} powercloud
+                GmbH, Offenburg.</p>
+
+            <a href="">
+                Impressum
+            </a>
+            <a href="">
+                Datenschutz
+            </a>
+
+            <a href="">
+                <span className="fa-stack fa-2x">
+                    <i className="fa-solid fa-square fa-stack-2x"></i>
+                    <i className={`${styles.icon} fa-brands fa-facebook fa-stack-1x fa-inverse`}></i>
+                </span>
+            </a>
+            <a href="">
+                <span className="fa-stack fa-2x">
+                    <i className="fa-solid fa-square fa-stack-2x"></i>
+                    <i className={`${styles.icon} fa-brands fa-xing fa-stack-1x fa-inverse`}></i>
+                </span>
+            </a>
+            <a href="">
+                <span className="fa-stack fa-2x">
+                    <i className="fa-solid fa-square fa-stack-2x"></i>
+                    <i className={`${styles.icon} fa-brands fa-linkedin fa-stack-1x fa-inverse`}></i>
+                </span>
+            </a>
+            <a href="">
+                <span className="fa-stack fa-2x">
+                    <i className="fa-solid fa-square fa-stack-2x"></i>
+                    <i className={`${styles.icon} fa-brands fa-twitter fa-stack-1x fa-inverse`}></i>
+                </span>
+            </a>
+
+            <a href="">
+                <span className="fa-stack fa-2x">
+                    <i className="fa-solid fa-square fa-stack-2x"></i>
+                    <i className={`${styles.icon} fa-brands fa-youtube fa-stack-1x fa-inverse`}></i>
+                </span>
+            </a>
+
+
+            <a href="">
+                <span className="fa-stack fa-2x">
+                    <i className="fa-solid fa-square fa-stack-2x"></i>
+                    <i className={`${styles.icon} fa-brands fa-instagram fa-stack-1x fa-inverse`}></i>
+                </span>
+            </a>
+
+        </div>
     );
 }
