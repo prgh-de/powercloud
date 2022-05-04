319e00d672829c335380478d39bca40590b567e5
adesso <gabriel.hoops@adesso.de>
2022-05-04 04:35:20 -0700

    add fonts and correct style->final beta

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,23 @@
 import styles from './Hero.module.css';
+import PropTypes from 'prop-types';
+
 export default function Hero({title, bodyText}) {
     return (
-        <div className={styles.container}>
         <div className={styles.heroContainer}>
-            <div className={styles.innerContainer}>
-                <div className={styles.headlineTitle}>
-                    <h2>{title}</h2>
-                </div>
-                <div className={styles.bodyText}>
-                    <p>
-                        {bodyText}
-                    </p>
+            <div className={styles.container}>
+                <div className={styles.innerContainer}>
+                    <h2 className={styles.headlineTitle}>{title}</h2>
+                    <div className={styles.bodyText}>
+                        <p>{bodyText}</p>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
     )
+}
+
+Hero.propTypes = {
+    title: PropTypes.string,
+    bodyText: PropTypes.string
 }
 
