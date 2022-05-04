017710a341b5989464d4c4a8ddf85db2901a2c35
msirisko <mirja.sirisko@adesso.de>
2022-05-04 03:12:52 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Components/Title/Title";
+import Title from "./Components/Title/Title";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="Title">
+          <Title />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,11 @@
+import style from "./Title.module.css"
+
+export default function Title() {
+    const text ="Die führende SaaS Plattform der Energiewirtschaft für Mid- und Backoffice-Prozesse."
+    return (
+        <div className={style.Title}>
+            <h1 className={style.Text}>
+                {text}
+            </h1>
+        </div>);
+}
