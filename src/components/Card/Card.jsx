561602a69dfb6b9ab5501e383e17183e2fa43e40
Bircan Gürmen <bircan.guermen@adesso.de>
2022-05-05 01:43:49 -0700

    set imgUrl manually

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,11 @@
 import styles from "./Card.module.css";
 
-export default function Card( {heading, text, linkText, linkUrl} ) {
+export default function Card( {heading, text, linkText, linkUrl, imgUrl} ) {
 
 
     return (
         <>
-            <div className={styles.card}>
+            <div className={styles.card} style={ {background: `url(${imgUrl}) bottom right no-repeat, linear-gradient(to bottom right,#ffd640,#ffcc12)`} }>
                 <h2>{heading}</h2>
                 <p>{text}</p>
                 <a href={linkUrl}>{linkText}</a>
--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -10,17 +10,20 @@ export default function CardContainer() {
                 heading="powercloud für Lieferanten"
                 text="5 der 6 größten deutschen Lieferanten, Stadtwerke und Energie-Startups setzen auf powercloud. Strom, Gas, Wasser, Abwasser, Wärme, E-Mobilität und Non-Commodity. Rund 8 Millionen Endkundenverträge werden heute bereits auf powercloud abgewickelt. CRM, Abrechnung, Marktkommunikation und Buchhaltung sind nur einige der über 80 eigenen und powerApps, die digitale Prozesse und Automatisierungen für Lieferanten bereitstellen."
                 linkText="Zur Lösung"
-                linkUrl="/"/>
+                linkUrl="/"
+                imgUrl="https://power.cloud/wp-content/uploads/2020/09/pc-home-box-bg-lieferant-verlauf_60percent_v02.min_.svg"/>
             <Card
                 heading="powercloud für Lieferanten"
                 text="5 der 6 größten deutschen Lieferanten, Stadtwerke und Energie-Startups setzen auf powercloud. "
                 linkText="Zur Lösung"
-                linkUrl="/"/>
+                linkUrl="/"
+                imgUrl="https://power.cloud/wp-content/uploads/2020/09/pc-home-box-bg-messstellenbetreiber_60percent_v02.min_.svg"/>
             <Card
                 heading="powercloud für Lieferanten"
                 text="5 der 6 größten deutschen Lieferanten, Stadtwerke und Energie-Startups setzen auf powercloud. Strom, Gas, Wasser, Abwasser, Wärme, E-Mobilität und Non-Commodity."
                 linkText="Zur Lösung"
-                linkUrl="/"/>
+                linkUrl="/"
+                imgUrl="https://power.cloud/wp-content/uploads/2020/09/pc-home-box-bg-netzbetreiber_60percent_v02.min_.svg"/>
         </section>
 
     )
