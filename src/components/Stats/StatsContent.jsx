3e3f0a606733b153b9addc362bbb730c08d5380c
adesso <gabriel.hoops@adesso.de>
2022-05-06 04:02:01 -0700

    version with class change

--- src/components/Stats/StatsContent.jsx
+++ src/components/Stats/StatsContent.jsx
@@ -1,4 +1,4 @@
-export default function StatsContent({title,text}) {
+export default function StatsContent({title,text,firstActive}) {
     return (
         <div>StatsContent</div>
     )
--- src/components/Stats/StatsNav.jsx
+++ src/components/Stats/StatsNav.jsx
@@ -1,8 +1,38 @@
 import styles from './StatsNav.module.css'
 export default function StatsNav({navElements}) {
+
+    function onClickNavElement(e) {
+        let element = e.target;
+
+        if (!element.classList.contains(styles.active)) {
+            element.classList.add(styles.active);
+            setActiveNavElement(element);
+            removeClassesNotActive(element);
+        }
+    }
+
+    function removeClassesNotActive(notRemovingElement) {
+        let allNavElements = document.getElementsByClassName(styles.navElement);
+        for (let i = 0;i<allNavElements.length;i++) {
+            if (allNavElements[i] !== notRemovingElement) {
+                allNavElements[i].classList.remove(styles.active);
+                navElements[i].active = false;
+            }
+        }
+    }
+    function setActiveNavElement(element) {
+        for (let i = 0;i<navElements.length;i++) {
+            if (navElements[i].name === element.id) {
+                navElements[i].active = true;
+            }
+        }
+    }
+
     return (
         <div className={styles.navContainer}>
-
+            {navElements.map((navElement,i)=>(
+                <div className={[styles.navElement,navElement.active ? styles.active : ''].join(' ')} key={i} onClick={onClickNavElement} id={navElement.name}>{navElement.name}</div>
+            ))}
         </div>
     )
 }
