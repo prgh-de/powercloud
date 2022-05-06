899a0b042b85b5d9d435de54c1f88c35ea9f934c
msirisko <mirja.sirisko@adesso.de>
2022-05-06 06:45:15 -0700

    button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./Components/Title/Title";
 import Features from "./Components/Features/Features"
+import Title from "./Components/Title/Title";
 
 function App() {
   return (
     <div className="App">
       <div className="Title">
+          <Title />
           <Features />
       </div>
     </div>
--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -4,22 +4,22 @@ export default function Features () {
     const header = "Darum lieben IT-Architekten powercloud:";
     const content ="Modular, „cloud-native“ und bewiesene Skalierbarkeit, dazu REST-Services, Events und Automatisierung mit Machine Learning.";
     const list = ["Moderne Softwarearchitektur", "Etablierte Standards", "Konsequenter cloud-basierter Ansatz", "Stetige Bereitstellung & Weiterentwicklung der Software"];
-    const print_list = list.map((item) => <p className={style.li}>{item}</p>);
-    const link = <img src="https://power.cloud/wp-content/themes/powercloud/img/icons/pc-icons/pc-icons-chevron-right.svg" alt="chevron"/>
+    const print_list = list.map((item) => <li className={style.li}>{item}</li>);
+    const linkText = "ERFAHRE MEHR ÜBER DIE PLATTFORM >"
 
     return(
         <div className={style.features}>
-            <h1 className={style.box}>
+            <div className={style.box}>
             <div className={style.container}>
                 <div className={style.left}>
-                    <h1 className={style.header}>{header}</h1>
+                    <h2 className={style.header}>{header}</h2>
                     <div className={style.content}>{content}</div>
                     <div className={style.list}>{print_list}</div>
-                    <div className={style.link}>{link}</div>
+                    <div className={style.link}>{linkText}</div>
                 </div>
-                <div className={style.right}><img src="https://power.cloud/wp-content/uploads/2020/09/ITEntwickler.png"  alt="IT-Entwickler" height="80%" width="80%" /></div>
+                <div className={style.right}><img className={style.img} src="https://power.cloud/wp-content/uploads/2020/09/ITEntwickler.png"  alt="IT-Entwickler" height="auto%" width="90%" /></div>
             </div>
-            </h1>
+            </div>
 
         </div>
     );
