4b9b1c9d363a1473401f871822b65c012e8f911a
msirisko <mirja.sirisko@adesso.de>
2022-05-07 04:34:38 -0700

    loop list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./Components/Title/Title";
 import Features from "./Components/Features/Features"
-import Title from "./Components/Title/Title";
 
 function App() {
   return (
     <div className="App">
       <div className="Title">
-          <Title />
+
           <Features />
       </div>
     </div>
