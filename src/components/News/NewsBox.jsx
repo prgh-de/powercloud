2f952a201c1494c34dfa6a13da4f893879d2ca2c
adesso <gabriel.hoops@adesso.de>
2022-05-17 01:16:37 -0700

    rework

--- src/components/News/NewsBox.jsx
+++ src/components/News/NewsBox.jsx
@@ -1,7 +1,7 @@
 import styles from './NewsBox.module.css';
 
 export default function NewsBox({article}) {
-    let boxstyle = defineClasses(article);
+    let boxstyle = article.boxstyle === 'small' ? styles.newsBoxSmall : styles.newsBoxBig;
     let button = article.buttonstyle === 'orange' ? styles.orangeButton : styles.blackButton;
 
     let style = {
@@ -9,42 +9,18 @@ export default function NewsBox({article}) {
         "backgroundColor": article.backgroundColor,
     }
 
-    function defineClasses (articel) {
-        let boxstyle = [];
-        boxstyle[0] = article.boxstyle === 'small' ? styles.newsBoxSmall : styles.newsBoxBig;
-        boxstyle[1] = articel.textAsOverlay === true ? styles.overlay : '';
-        let backgroundClass = `backgroundClass${articel.id}`;
-        let head = document.getElementsByTagName('head');
-        checkDuplicateStyle(head[0]);
+    function showOverlay(e) {
 
-        if (document.getElementsByClassName(backgroundClass).length === 0) {
-            let style = document.createElement('style');
-            let backgroundImage = article.backgroundImage !== 'none' ? `url(${article.backgroundImage})`:'none';
-            style.innerHTML = ` .${backgroundClass} background-color: ${articel.backgroundColor}; background-image: ${backgroundImage}`;
-
-            head[0].appendChild(style);
-            boxstyle[2] = backgroundClass;
-        }
-
-
-        return boxstyle.join(' ');
     }
-    function checkDuplicateStyle(elem) {
-        for(let i = 0;i<elem.children.length;i++) {
-            if (elem.children[i].tagName != 'STYLE') {
-                continue;
-            }
-            console.log(elem.children[i]);
-        }
-    }
-
 
     return (
-        <div className={boxstyle}>
-            <div className={styles.title}>{article.title}</div>
-            <div className={styles.subtitle}><a href={article.subtitlelink}>{article.subtitle}</a></div>
-            <div className={styles.text}>{article.text}</div>
-            <div className={button}><a href={article.subtitlelink}><span className={styles.nextIcon}></span></a></div>
+        <div className={boxstyle} style={style}>
+            <div className={styles.overlay} onMouseOver={showOverlay} onMouseOut={showOverlay} id={`newsbox${article.id}`}>
+                <div className={styles.title}>{article.title}</div>
+                <div className={styles.subtitle}><a href={article.subtitlelink}>{article.subtitle}</a></div>
+                <div className={styles.text}>{article.text}</div>
+                <div className={button}><a href={article.subtitlelink}><span className={styles.nextIcon}></span></a></div>
+            </div>
         </div>
     )
 }
