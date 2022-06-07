3ff443079542412117e45630303b0cd43b4bcd47
msirisko <mirja.sirisko@adesso.de>
2022-06-07 01:00:47 -0700

    form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import {Route, Routes} from "react-router-dom";
-import RouteLocation from "./pages/RouteLocation";
+import ContactForm from "./Components/ContactForm";
+import useContacts from "./Components/useContacts";
+import DataSent from "./Components/DataSent";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     <Routes>
-        <Route path="/location" element={<RouteLocation />} />
-     </Routes>
-    </div>
-  );
+    const [contacts, addContact] = useContacts();
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<ContactForm sendDataOnSubmit={addContact}/>} />
+                    <Route path="/datasent" element={<DataSent />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Components/Components/ContactApiService.jsx
@@ -0,0 +1,16 @@
+export async function addContactToApi(contact) {
+    const httpResponse = await fetch('http://localhost:4000/contacts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: new Date(), ...contact }),
+    });
+    const data = await httpResponse.json();
+
+    if (data) {
+        console.log('success ' + data);
+    } else {
+        console.error('error ' + data);
+    }
+}
