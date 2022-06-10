5593573c90838f44fa0183997d84389457fba9a2
Adrian Rinnus <adrian.rinnus@prgh.com>
2022-06-10 09:58:12 +0200

    Add Testimonials to Home page.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero/Hero";
 import Features from "../Components/Features/Features";
 import Customers from "../components/Customers/Customers";
+import Testimonials from "../Components/Testimonials/Testimonials";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
                       content ="Modular, „cloud-native“ und bewiesene Skalierbarkeit, dazu REST-Services, Events und Automatisierung mit Machine Learning."
                       list = {["Moderne Softwarearchitektur", "Etablierte Standards", "Konsequenter cloud-basierter Ansatz", "Stetige Bereitstellung & Weiterentwicklung der Software"]}
                       linkText = "Erfahre mehr über die Plattform  >"  />
+            <Testimonials />
         </div>
     );
 }
